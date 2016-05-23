8f65d55ca81504dd18f81b9da39c64656196e35f
Eric Entin <eric@ericent.in>
2016-05-23 13:32:55 -0400

    Upgrade erlexec to 1.1.3

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Exexec.Mixfile do
 
   defp deps do
     [
-      {:erlexec, "~> 1.1.2"},
+      {:erlexec, "~> 1.1.3"},
       {:dialyxir, "~> 0.3", only: [:dev, :test]},
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, "~> 0.11", only: :dev},
