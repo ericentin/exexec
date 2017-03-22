adfe1cc951c12a5658ff2026bed4b1ba8273f19c
Eric Entin <whoknew@gmail.com>
2017-03-22 15:47:03 -0400

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Exexec.Mixfile do
     [
       app: :exexec,
       version: @version,
-      elixir: "~> 1.4",
+      elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
